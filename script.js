e2b7761adf6cdf068ef193f96b0ad50f4dbc7cad
tumblewade <wkohel09@gmail.com>
2024-12-17 10:43:19 -0600

    Added js

--- script.js
+++ script.js
@@ -21,5 +21,20 @@ para.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
 
 sect.appendChild(para);
 
+const text = document.createTextNode(' — the premier source for web development knowledge.');
 
+const linkPara = document.querySelector('p');
+linkPara.appendChild(text);
+
+sect.appendChild(linkPara);
+
+sect.removeChild(linkPara);
+
+linkPara.remove();
+
+para.style.color = 'white';
+para.style.backgroundColor = 'black';
+para.style.padding = '10px';
+para.style.width = '250px';
+para.style.textAlign = 'center';
 
