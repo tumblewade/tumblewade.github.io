e0b4e965d3cdeebba39d76e5314e4c1116c97fce
tumblewade <wkohel09@gmail.com>
2024-12-17 10:46:01 -0600

    changed

--- script.js
+++ script.js
@@ -19,6 +19,12 @@ const sect = document.querySelector('section');
 const para = document.createElement('p');
 para.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
+para.style.color = 'white';
+para.style.backgroundColor = 'black';
+para.style.padding = '10px';
+para.style.width = '250px';
+para.style.textAlign = 'center';
+
 sect.appendChild(para);
 
 const text = document.createTextNode(' — the premier source for web development knowledge.');
@@ -32,9 +38,5 @@ sect.removeChild(linkPara);
 
 linkPara.remove();
 
-para.style.color = 'white';
-para.style.backgroundColor = 'black';
-para.style.padding = '10px';
-para.style.width = '250px';
-para.style.textAlign = 'center';
+
 
