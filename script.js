80a636a1e858b3a221d327152c2425d721815f2d
tumblewade <wkohel09@gmail.com>
2025-01-14 23:12:14 +0000

    Added Style

--- script.js
+++ script.js
@@ -14,4 +14,27 @@ function myFunction() {
   }
 }
 
+Plotly.d3.csv('https://tumblewade.github.io/openingData.csv', function(err, rows) {
+  if (err) {
+      console.error('Error loading CSV:', err);
+      return;
+  }
 
+  console.log('Loaded data:', rows);
+
+  function unpack(rows, key) {
+      return rows.map(function(row) { return row[key]; });
+  }
+
+  var data = [{
+      type: 'bar',
+      x: unpack(rows, 'Opening Move'),
+      y: unpack(rows, '# of Games'),
+  }];
+
+  var layout = {
+      title: 'Games Played per Opening Move',
+  };
+
+  Plotly.plot('barPlot', data, layout);
+});
